6d74139a88e6bf7918302d88e389bc6789c516df
Anthony Stolp <anthony.stolp@gmail.com>
2023-09-12 09:37:12 -0400

    pushing to master, dont try this at home kids

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import "./App.css";
 import Nav from "./components/nav/nav";
 import useFetch from "./hooks/api";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./layouts/home";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { prefersDarkMode } from "./utilities/utilities";
 import Services from "./layouts/services";
 import About from "./layouts/about";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    setIsLoading(true); // Start with loading animation
+
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false); // End loading animation
+      clearTimeout(loadingTimeout);
+    }, 500); // Adjust the duration as needed
+
+    return () => {
+      clearTimeout(loadingTimeout);
+    };
+  }, [location]);
+
   useEffect(() => {
     if (prefersDarkMode()) {
       document.documentElement.classList.add("dark"); // Apply dark mode globally
@@ -23,14 +39,14 @@ function App() {
   }
 
   return (
-    <>
+    <div className="app fade-in-animation">
       <Nav data={fetchedNavData} />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
