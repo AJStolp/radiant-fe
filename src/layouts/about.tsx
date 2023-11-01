5e1e17c1fc271ca655963787fbca64c256ace9be
Anthony Stolp <47982927+AJStolp@users.noreply.github.com>
2023-11-01 15:15:24 -0400

    About page tweaks (#14)
    * committing change
    
    * about page changes

--- src/layouts/about.tsx
+++ src/layouts/about.tsx
@@ -2,72 +2,40 @@ import useFetch from "../hooks/use-fetch";
 
 export default function About() {
   const { fetchedAboutData, error } = useFetch({
-    url: "/api/abouts?populate=*",
+    url: "/api/abouts?populate[0]=teaminfo&populate[1]=teaminfo.employeephoto&populate[2]=teaminfo.employeephoto.media",
   });
   if (error) {
     return <p>{error}</p>;
   }
 
   return (
-    <section className='grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-xl m-auto pt-16'>
+    <section className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-xl m-4 lg:m-auto lg:pt-16">
       {fetchedAboutData.flatMap((item, outerIndex) => {
-        return item.attributes.teaminfo.map((team, innerIndex) => (
-          <div key={`${outerIndex}-${innerIndex}`}>
-            <div className='text-center text-light-text dark:text-dark-text'>
-              <img
-                className='mx-auto mb-4 w-36 h-36 rounded-full'
-                src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png'
-                alt='Bonnie Avatar'
-              />
-              <h3 className='mb-1 text-2xl  tracking-tight dark:text-white'>
-                {team.name}
-              </h3>
-              <p>{team.about}</p>
-              {/* <ul className="flex justify-center mt-4 space-x-4">
-                <li>
-                  <a href="#">
-                    <svg
-                      className="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                    ></svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="">
-                    <svg
-                      className="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                    ></svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="">
-                    <svg
-                      className="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                    ></svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-[#ea4c89] ">
-                    <svg
-                      className="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                    ></svg>
-                  </a>
-                </li>
-              </ul> */}
+        return item.attributes.teaminfo.map((team, innerIndex) => {
+          // Construct the image URL for each team member
+          const backendUrl = import.meta.env.VITE_BE_API_URL;
+          const imageUrl = `${backendUrl}${team.employeephoto.data.attributes.url}`;
+
+          return (
+            <div key={`${outerIndex}-${innerIndex}`}>
+              <div className="text-center text-light-text dark:text-dark-text">
+                <figure>
+                  <img
+                    className="mx-auto mb-4 w-fit h-fit rounded"
+                    src={imageUrl}
+                    alt={`${team.name} Avatar`}
+                  />
+                  <figcaption>
+                    <h3 className="mb-1 text-2xl tracking-tight text-light-accent">
+                      {team.name}
+                    </h3>
+                    <p>{team.about}</p>
+                  </figcaption>
+                </figure>
+              </div>
             </div>
-          </div>
-        ));
+          );
+        });
       })}
     </section>
   );
