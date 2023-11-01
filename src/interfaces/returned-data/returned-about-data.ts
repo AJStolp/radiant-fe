ed245f71e576e2aaa024a1f4693b75db669fc165
Anthony Stolp <anthony.stolp@gmail.com>
2023-11-01 15:14:48 -0400

    about page changes

--- src/interfaces/returned-data/returned-about-data.ts
+++ src/interfaces/returned-data/returned-about-data.ts
@@ -1,6 +1,64 @@
+interface Formats {
+  small: {
+    ext: string;
+    url: string;
+    hash: string;
+    mime: string;
+    name: string;
+    path: string | null;
+    size: number;
+    width: number;
+    height: number;
+  };
+  thumbnail: {
+    ext: string;
+    url: string;
+    hash: string;
+    mime: string;
+    name: string;
+    path: string | null;
+    size: number;
+    width: number;
+    height: number;
+  };
+}
+
+interface Attributes {
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface EmployeePhotoData {
+  id: number;
+  attributes: Attributes;
+}
+
+interface EmployeePhoto {
+  data: EmployeePhotoData;
+}
+
 export interface ReturnedAboutDataProps {
   id: number;
   attributes: {
-    teaminfo: [{ id: number; name: string; about: string }];
+    teaminfo: Array<{
+      id: number;
+      name: string;
+      about: string;
+      employeephoto: EmployeePhoto;
+    }>;
   };
 }
